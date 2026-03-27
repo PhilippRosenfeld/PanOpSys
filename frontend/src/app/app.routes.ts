cccaddc52f60201b32387f0dea5bebac9b7347b2
Philipp Rosenfeld <e12314158@student.tuwien.ac.at>
2026-03-27 15:39:55 +0100

    removed frontend, moved to main-app

--- frontend/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import {AtmoComponent} from './component/atmo-component/atmo-component';
-
-export const routes: Routes = [
-  {path: 'atmo', children: [
-    {path: '', component: AtmoComponent},
-    ]},
-];
